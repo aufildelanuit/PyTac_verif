d5f665b546202796e932a8c75e8ac00e1b3959aa
aufildelanuit <yohann.opolka@gmail.com>
2021-08-01 17:28:39 +0200

    Trying the sign argument for development

--- PyTac_verif.py
+++ PyTac_verif.py
@@ -1,7 +1,7 @@
 '''
 PyTac_verif.py
 
-a free tool to verify Covid-19 vaccination certificate signature (ECDSA) 
+a free tool to verify Covid-19 vaccination certificate signature (ECDSA)
 
 Laurent Clevy (@Lorenzo2472)
 
@@ -44,7 +44,7 @@ def parse_certificate(data):
     print('error no 0x1f marker')
     return
   else:
-    msg = bin_data[:sign_offset]  
+    msg = bin_data[:sign_offset]
     signature = bin_data.strip()[sign_offset+1:]
     signature = signature + (8-len(signature)%8)*b'=' #base32 padding
     bsign = b32decode(signature)
@@ -73,13 +73,13 @@ def parse_TSL(tls):
             2_ServiceDigitalIdentity
               0_DigitalId
                 0_X509Certificate
-    2_Signature' 
+    2_Signature'
   '''
   root = ET.fromstring( tls )
   print('%d trusted services providers found' % len(root[1]) )
-  
+
   #print(root[2]) #signature
-  
+
   tsp = dict()
   for p in root[1]: #TrustServiceProviderList
     TSPTradeName = p[0][1][0].text #TrustServiceProvider.TSPInformation.TSPTradeName = FR03
@@ -94,7 +94,7 @@ def parse_TSL(tls):
 def parse_certdb(db):
   tag_start = b'Content-Type: application/pkix-cert\x0d\x0a\x0d\x0a'
   tag_end = b'\x0d\x0a--End'
-  
+
   name = b'CAISSE NATIONALE D\'ASSURANCE MALADIE'
 
   start = 0
@@ -102,22 +102,22 @@ def parse_certdb(db):
     start = allcerts.find( tag_start, start )
     if start >= 0:
       start_cert = start+len(tag_start)
-      endp = allcerts.find( tag_end, start_cert ) 
+      endp = allcerts.find( tag_end, start_cert )
       if endp > 0:
         cert  = allcerts[start_cert:endp]
-        if cert.find( name )>=0: 
-          issuer_cn, subject_cn, pubkey_der = parse_der_cert( cert ) 
+        if cert.find( name )>=0:
+          issuer_cn, subject_cn, pubkey_der = parse_der_cert( cert )
           cert_filename = '%s%s_CERT.DER' % ( issuer_cn, subject_cn )
           with open(cert_filename, 'wb') as certf:
             print('saving %s...' % cert_filename)
             certf.write( cert )
         start = endp + len(tag_end)
       else:
-        print('?')      
+        print('?')
         start = start+1
-  
+
 def convert_date(d): #number of days since 1jan2000
-  return date(2000,1,1) + timedelta( int(d,16) ) 
+  return date(2000,1,1) + timedelta( int(d,16) )
 
 
 '''
@@ -131,15 +131,15 @@ seq
       0 C
       1 O
       2 OU
-      3 CN = FR03   
+      3 CN = FR03
     4 validity
     5 subject (seq)
       0 C
       1 O
       2 OU
-      3 
+      3
         0 obj_id
-        1 UTF8 AV01   
+        1 UTF8 AV01
     6 subject pubkey info (seq)
       0 seq
         0 obj_id 1.2.840.10045.2.1 ecPublicKey
@@ -151,14 +151,14 @@ seq
 '''
 def parse_der_cert( der ):
   cert_der = DerSequence().decode( der )
-  #assert DerObjectId().decode( DerSequence().decode(DerSequence().decode( DerSequence().decode(cert_der[0])[6] )[0] )[0] ).value == 
-  
+  #assert DerObjectId().decode( DerSequence().decode(DerSequence().decode( DerSequence().decode(cert_der[0])[6] )[0] )[0] ).value ==
+
   issuer_cn = DerSequence().decode(cert_der[0])[3][-4:].decode('ascii')
   subject_cn = DerSequence().decode(cert_der[0])[5][-4:].decode('ascii')
   assert DerObjectId().decode( DerSequence().decode(DerSequence().decode( DerSequence().decode(cert_der[0])[6] )[0] )[0] ).value == '1.2.840.10045.2.1' #ecPublicKey
   assert DerObjectId().decode( DerSequence().decode(DerSequence().decode( DerSequence().decode(cert_der[0])[6] )[0] )[1] ).value == '1.2.840.10045.3.1.7' #prime256v1
   pubkey_der =  DerSequence().decode(cert_der[0])[6]
-  return issuer_cn, subject_cn, pubkey_der  
+  return issuer_cn, subject_cn, pubkey_der
 
 
 def dl(url):
@@ -203,25 +203,25 @@ if content[:4]==b'DC04': #only Datacode v4
   print( 'cert', content[4:12] )
   pubkey = '%s_PUB.PEM' % content[4:12].decode('ascii')
   cert_name = '%s_CERT.DER' % content[4:12].decode('ascii')
-  
-  if args.update: #TO DO: verify signature of all files and CERTs ! 
-  
+
+  if args.update: #TO DO: verify signature of all files and CERTs !
+
     #get Trusted Service providers list and process it
-    print('\n[+]updating Trusted Service providers list')    
-    tsl = dl( TSL_url ) 
+    print('\n[+]updating Trusted Service providers list')
+    tsl = dl( TSL_url )
     if tsl is None:
       print('falling back to local copy, network is broken')
       with open(TSL_filename) as tslf:
         tsl = tslf.read()
     else:
-      print('overwriting local %s' % TSL_filename)    
+      print('overwriting local %s' % TSL_filename)
       with open(TSL_filename, 'wb') as tslf:
         tslf.write( tsl )
     tsp = parse_TSL( tsl ) #parse Trusted Services List
 
-    #process cert database for relevant issuer 
+    #process cert database for relevant issuer
     print('\n[+]extracting AV certs from issuer %s' % content[4:8].decode('ascii') )
-    allcerts = dl( tsp[content[4:8].decode('ascii')].info_uri ) 
+    allcerts = dl( tsp[content[4:8].decode('ascii')].info_uri )
     if allcerts is None:
       print('falling back to local copy, network is broken')
       with open('allcerts - FR03.der', 'rb') as allcertsf:
@@ -250,32 +250,33 @@ if args.verify:
         print ("The message is not authentic.")
   except FileNotFoundError:
     print('certificate %s not found, maybe you need to update (-u) ?' % cert_name)
-    
-'''
+
+
 #broken yet
 if args.sign:
   privkey = '%s_PRIV.PEM' % content[4:12].decode('ascii')
   with open( privkey ) as privf:
     key = ECC.import_key( privf.read() )
     signer = DSS.new(key, 'deterministic-rfc6979')
-    signature = signer.sign( SHA256.new(msg) )  
+    signature = signer.sign( SHA256.new(msg) )
     print(SHA256.new(msg).hexdigest())
     print(hexlify(signature))
     print(hexlify(bsign))
-'''
+
+
+
 #requires openssl installed
 if args.verify and openssl_check:
   print('\ntesting with openssl')
-  
+
   r = bsign[:32]
   s = bsign[32:]
   der_sign = DerSequence( [DerInteger(int.from_bytes(r, 'big')), DerInteger(int.from_bytes(s, 'big'))] ).encode()
   with open('sign.der', 'wb') as signf:
     signf.write( der_sign )
-   
+
   with open('msg.hex', 'wb') as msgf:
     msgf.write(msg )
 
   system('openssl x509 -in %s -inform DER -pubkey -outform PEM -out %s' % (cert_name, pubkey))
   system('openssl dgst -sha256 -verify %s -signature sign.der msg.hex' % pubkey)
-
